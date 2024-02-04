5006ec6b94d0a6bf0e6bdd47bf03ac4cd5816843
NishantGupt786 <nishantgupta2325@gmail.com>
2024-02-04 22:39:26 +0530

    fix:delete from watchlist

--- api/routes/userActions.js
+++ api/routes/userActions.js
@@ -4,7 +4,7 @@ const verifyAccessToken = require("../middlewear/jwtMiddlewear")
 const actionsController = require("../controllers/actionsController")
 
 router.put("/add-to-watchlist/:id", verifyAccessToken, actionsController.addWatchlist)
-router.put("/remove-from-watchlist/:id", verifyAccessToken, actionsController.removeWatchlist)
+router.delete("/remove-from-watchlist/:id", verifyAccessToken, actionsController.removeWatchlist)
 router.put("/buy-course/:id", verifyAccessToken, actionsController.buyCourse)
 router.get("/get-watchlist", verifyAccessToken, actionsController.getWatchlist)
 router.get("/get-purchased", verifyAccessToken, actionsController.getBought)
